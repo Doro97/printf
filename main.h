028f18bedf0914c3b9e5439fe4a987972ec0bcca
dorothy <dorothycheruiyot97@gmail.com>
2022-04-19 21:21:16 +0300

    va _list argument

--- main.h
+++ main.h
@@ -1,2 +1,22 @@
 
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+// #include <unistd.h>
+
+/**
+ * struct types - specifiier structure for printf
+ * @p: pointer to character specifier
+ * @funct : function pointer to print functions
+ */
+
+typedef struct types
+{
+    char p;
+    int (*func)(va_list);
+}print_f;
+
 int _printf(const char *format, ...);
